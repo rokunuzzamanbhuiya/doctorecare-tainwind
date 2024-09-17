f040108ae21b0c1fad5ca7f4506c4d230afbca33
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2024-09-17 09:52:04 +0600

    fonts and icons added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./dist/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-green": "#00856F",
+        "green-light": "#DCE9E2",
+        "green-light-02": "#F7F9F9",
+        "brand-beige": "#FFFAF1",
+        "green-hover": "#00453A",
+      },
+      fontFamily: {
+        "primary": ["DM Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
